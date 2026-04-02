419a4d702e56ce9112c527bf90e9328b259a9146
Lucas <morganis1411@gmail.com>
2026-04-01 23:11:15 -0300

    feat: adjusting variables

--- modules/eks-nodegroup/variables.tf
+++ modules/eks-nodegroup/variables.tf
@@ -1,7 +1,14 @@
-# Variable for the CIDR BLOCK to be used for the VPC
+#============================================
+# Global Variables
+#============================================
 variable "project_name" {
   type        = string
   description = "Project name to be used to name the resources (name tag)"
+}
+
+variable "environment" {
+  description = "Environment (Homologação, Produção, etc)"
+  type        = string
 }
 
 variable "tags" {
@@ -15,12 +22,14 @@ variable "key-pair" {
 }
 
 
+#============================================
+# EKS Node Group Variables
+#============================================
 # Cluster name
 variable "cluster_name" {
   type        = string
   description = "Cluster name to be used for integrate mng to cluster"
 }
-
 
 # Private subnets to create the MNG. From Network Module
 variable "private_subnet_ids" {
@@ -28,6 +37,7 @@ variable "private_subnet_ids" {
   description = "Subnet IDs to create EKS manage node group"
 }
 
+# EKS Node Group Security Group
 variable "eks_cluster_sg" {
   type        = string
   description = "Cluster SG to ingress rules"
