db99b5ac262d057000a48e8436a40e91e36a01f5
Lucas <morganis1411@gmail.com>
2026-04-09 16:22:54 -0300

    feaT: add network variables

--- modules/network/variables.tf
+++ modules/network/variables.tf
@@ -1,21 +1,27 @@
 #============================================
+# Global Variables
+#============================================
+variable "project_name" {
+  description = "Project name for resource naming"
+  type        = string
+}
+
+variable "environment" {
+  description = "Environment (Homologação, Produção, etc)"
+  type        = string
+}
+
+variable "tags" {
+  description = "Tags for the RDS instance"
+  type        = map(string)
+  default     = {}
+}
+
+#============================================
 # Variables to be used for the network module
 #============================================
-
 # Variable for the CIDR BLOCK to be used for the VPC
 variable "cidr_block" {
   type        = string
   description = "Networking CIDR block to be used for the VPC"
-}
-
-# Variable for the project name
-variable "project_name" {
-  type        = string
-  description = "Project name to be used to name the resources (name tag)"
-}
-
-# Variable for the tags
-variable "tags" {
-  type        = map(any)
-  description = "Tags to be added to AWS resources"
 }
